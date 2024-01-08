488fc6a27cf151ac0f9f93d37511e4d37bf82a86
Vector_scope <gsball399@gmail.com>
2024-01-08 11:14:35 +0900

    :tada: [Feature] :: 기본 프로젝트

---
+++ QVick/QVickApp.swift
@@ -0,0 +1,17 @@
+//
+//  QVickApp.swift
+//  QVick
+//
+//  Created by dgsw8th16 on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct QVickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QVick/Sources/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QVick
+//
+//  Created by dgsw8th16 on 1/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
