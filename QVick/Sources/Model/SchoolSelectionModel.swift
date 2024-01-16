e1ef3b318e3f336c9a0b1c02db0966eeb23b1c1b
lsh1202 <vic071202@gmail.com>
2024-01-16 12:46:19 +0900

    :sparkles: [Feature] :: 학교 선택 모델, 학교 선택 뷰모델 개발

---
+++ QVick/Sources/Model/SchoolSelectionModel.swift
@@ -0,0 +1,21 @@
+//
+//  SchoolSelectionModel.swift
+//  QVick
+//
+//  Created by dgsw8th32 on 1/16/24.
+//
+
+import Foundation
+
+struct SchoolSelectionModel {
+  
+    var textFieldContent: String
+    
+    var schoolList: [SchoolModel]?
+}
+
+struct SchoolModel {
+    
+    var schoolName: String
+    var schoolAddress: String
+}
---
+++ QVick/Sources/ViewModel/SchoolSelectionViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  SchoolSelectionViewModel.swift
+//  QVick
+//
+//  Created by dgsw8th32 on 1/16/24.
+//
+
+import Foundation
+
+class SchoolSelectionViewModel: ObservableObject {
+    @Published var school = SchoolSelectionModel(textFieldContent: "")
+    
+    var content: String {
+        var textFieldContnet = school.textFieldContent
+        return textFieldContnet
+    }
+    
+    var schoolList: [SchoolModel]? {
+        var schoolList = school.schoolList
+        
+        return schoolList
+    }
+}
