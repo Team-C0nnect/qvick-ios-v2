0f569fbf81131e6c29b9acd4a10412fab0beff45
Vector_scope <gsball399@gmail.com>
2024-01-17 11:47:57 +0900

    :sparkles: PersonalDataView 개발

--- QVick/Sources/ViewModel/PersonalDataViewModel.swift
+++ QVick/Sources/ViewModel/PersonalDataViewModel.swift
@@ -6,14 +6,21 @@
 //
 
 import SwiftUI
+import Alamofire
 
 class PersonalDataViewModel: ObservableObject {
+    
     @Published var personModel: [PersonalDataModel] = [
-        PersonalDataModel(label: "개인정보 처리방침", isClicked: false, url: "")
+        PersonalDataModel(label: "서비스 이용약관 (필수)", url: ""),
+        PersonalDataModel(label: "개인정보 수집 / 이용 동의 (필수)", url: "")
     ]
     
     func getClickedSelection(_ index: Int) {
         self.personModel[index].isClicked.toggle()
+    }
+    
+    func getHtmlData() {
+        print("getHtmlData")
     }
     
 }
