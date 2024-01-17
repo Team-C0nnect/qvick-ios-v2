2b3b70abcc7fd9b88756d262cc73bcfae50fb9d8
jyw28 <roy03280328@gmail.com>
2024-01-17 15:40:16 +0900

    ✨ [Feature] :: Add NextButton, ViewExt

---
+++ QVick/Sources/Extenstion/ViewExt.swift
@@ -0,0 +1,20 @@
+//
+//  ViewExt.swift
+//  QVick
+//
+//  Created by dgsw8th36 on 1/17/24.
+//
+
+import SwiftUI
+
+extension View {
+    
+    func nextButton(destination: some View) -> some View {
+        NextButton() {
+            AnyView(self)
+        } destination: {
+            AnyView(destination)
+        }
+    }
+    
+}
