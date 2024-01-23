f7a26d3d9732a3d5db23e8f4d5be32b0923aba41
lsh1202 <vic071202@gmail.com>
2024-01-23 17:33:36 +0900

    :sparkles: [Feature] :: 외출/외박 신청 뷰 및 컴포넌트 개발

---
+++ QVick/Sources/Components/ReasonCell.swift
@@ -0,0 +1,36 @@
+//
+//  Reason Cell.swift
+//  QVick
+//
+//  Created by dgsw8th32 on 1/23/24.
+//
+
+import SwiftUI
+
+struct ReasonCell: View {
+    @State var label: String = ""
+    @State var title: String = ""
+    @State var placeholder: String = ""
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.judson(.bold, 15))
+                .foregroundStyle(.white)
+            
+            Spacer()
+            
+            TextField(placeholder, text: $label)
+                .frame(width: 245, height: 35)
+                .background(Color(uiColor: .secondarySystemBackground))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .padding(.vertical, 5)
+        .padding(.horizontal, 30)
+    }
+}
+
+#Preview {
+    ReasonCell(title: "외박 사유", placeholder: " 외박사유를 적어주세요")
+        .background(Color.blueGray)
+}
