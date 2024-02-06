2fef5209b546a48cd7b327eca16425d1e950160e
jyw28 <roy03280328@gmail.com>
2024-02-06 18:15:38 +0900

    ✨ [Feature] :: 프로필 정보 받는 뷰에서 서버에 정보 보내주는 기능 만들기, nextButton에 action 추가

--- QVick/Sources/Extenstion/ViewExt.swift
+++ QVick/Sources/Extenstion/ViewExt.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 extension View {
     
-    func nextButton(destination: some View) -> some View {
+    func nextButton(destination: some View, action: (() -> ())? = nil ) -> some View {
+        
         NextButton() {
             AnyView(self)
         } destination: {
             AnyView(destination)
+        } action: {
+            if let action = action {
+                action()
+            } else {
+                print("error")
+            }
         }
     }
     
